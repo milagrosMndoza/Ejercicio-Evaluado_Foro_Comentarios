4ab6c6ce3f3895fef2a229a111a352976f986e00
milagrosMndoza <milagrosmndoza@gmail.com>
2017-10-26 12:27:26 -0500

    subiendo trabajo

---
+++ src/actions.js
@@ -0,0 +1,19 @@
+import  store from './store'
+
+  export const addComment = (name, comment) =>{
+   const comments = [...store.getState().comments]
+   const newComment= comments.concat( {
+         name: name,
+         comment: comment
+  });
+  store.setState({
+        comments: newComment
+})
+}
+
+ export const deleteComment = (index) =>{
+	const comments =  store.getState().comments.filter( (item, id) => id != index );
+	store.setState({
+		comments: comments
+	})
+  }
---
+++ src/store.js
@@ -0,0 +1,17 @@
+import createStore from "redux-zero";
+
+export const COMMENT = [];
+    //  this.index = 0;
+    
+// export const inputName = null;
+// export const inputComent = null;
+    
+let index = 0;
+ 
+const initialState={
+index:index,
+comments:COMMENT
+    
+}
+const store = createStore(initialState);
+export default store
